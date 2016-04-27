6c11b759cb18f26074591c2997ede392270a9641
Alshev Pavel <alshev.pavel@yandex.ru>
2016-04-27 16:56:36 +0300

    Player created

---
+++ src/io/hexlet/xo/model/Player.java
@@ -0,0 +1,26 @@
+package io.hexlet.xo.model;
+
+/**
+ * Created by pasha_molodoy on 27.04.16.
+ */
+public class Player {
+
+    private final String name;
+
+    private final Figure figure;
+
+    public Player(final String name,
+                  final Figure figure) {
+        this.name = name;
+        this.figure = figure;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+}
